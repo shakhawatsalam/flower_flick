694341d8f9fb702d4b0270f22b4b59d48a06a8fd
Shakhawat Salam <shakhawatsalam726@gmail.com>
2025-05-10 13:18:09 +0600

    [Preparig for final productio03 CORS]

--- src/redux/features/api/baseApi.js
+++ src/redux/features/api/baseApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_BASE_API,
+    baseUrl: "https://flowerflick.vercel.app/api/v1",
     credentials: "include",
     prepareHeaders: (headers) => {
       // Retrieve the JWT token from localStorage
